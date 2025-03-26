6c2689f49af50ebfc8fc39a0d877d65758ced539
rparkerrii <rparkerrii@gmail.com>
2025-03-26 03:12:15 -0700

    Front end updated UI
    started to add some cleaner UI

--- frontend/src/AnimalForm.jsx
+++ frontend/src/AnimalForm.jsx
@@ -30,16 +30,26 @@ const AnimalForm = ({ existingAnimal = {}, updateCallback }) => {
 
     return (
         <form onSubmit={onSubmit}>
-            <div>
-                <label htmlFor="name">Animal Name:</label>
+            <div className="input-container">
                 <input
+                    placeholder="Search for animal..." 
+                    name="text" 
+                    className="input"
                     type="text"
                     id="name"
                     value={name}
                     onChange={(e) => setName(e.target.value)}
                 />
+                <button className="button-enter" type="submit">
+                    <div className="button-enter-box">
+                        <span className="button-enter-elem">
+                            <svg viewBox="0 0 46 40" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M46 20.038c0-.7-.3-1.5-.8-2.1l-16-17c-1.1-1-3.2-1.4-4.4-.3-1.2 1.1-1.2 3.3 0 4.4l11.3 11.9H3c-1.7 0-3 1.3-3 3s1.3 3 3 3h33.1l-11.3 11.9c-1 1-1.2 3.3 0 4.4 1.2 1.1 3.3.8 4.4-.3l16-17c.5-.5.8-1.1.8-1.9z"></path>
+                            </svg>
+                        </span>
+                    </div>
+                </button>
             </div>
-            <button type="submit">{updating ? "Update" : "Create"}</button>
         </form>
     );
 };
